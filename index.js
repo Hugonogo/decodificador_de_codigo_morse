367abb86e2a49372d36df902787dcd130e50e6ec
Hugo <112352124+Hugonogo@users.noreply.github.com>
2023-01-19 09:47:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,47 +7,47 @@ let resultText = document.querySelector("#criptresult");
 
 
 function criptografar() {
-    let morse = text.value;
+   let morse = text.value;
     //Alfabeto
-    morse = morse.replace(/A/gi, ".-");
-    morse = morse.replace(/B/gi, "-...");
-    morse = morse.replace(/C/gi, "-.-.");
-    morse = morse.replace(/D/gi, "-..");
-    morse = morse.replace(/E/gi, ".");
-    morse = morse.replace(/F/gi, "..-.");
-    morse = morse.replace(/G/gi, "--.");
-    morse = morse.replace(/H/gi, "....");
-    morse = morse.replace(/I/gi, "..");
-    morse = morse.replace(/J/gi, ".---");
-    morse = morse.replace(/K/gi, "-.-");
-    morse = morse.replace(/L/gi, ".-..");
-    morse = morse.replace(/M/gi, "--");
-    morse = morse.replace(/N/gi, "-.");
-    morse = morse.replace(/O/gi, "---");
-    morse = morse.replace(/P/gi, ".--.");
-    morse = morse.replace(/Q/gi, "--.-");
-    morse = morse.replace(/R/gi, ".-.");
-    morse = morse.replace(/S/gi, "...");
-    morse = morse.replace(/T/gi, "-");
-    morse = morse.replace(/U/gi, "..-");
-    morse = morse.replace(/V/gi, "...-");
-    morse = morse.replace(/W/gi, ".--");
-    morse = morse.replace(/X/gi, "-..-");
-    morse = morse.replace(/Y/gi, "-.--");
-    morse = morse.replace(/Z/gi, "--..");
-    morse = morse.replace(/ /gi, "/");
+    morse = morse.replace(/A/gi, ".- ");
+    morse = morse.replace(/B/gi, "-... ");
+    morse = morse.replace(/C/gi, "-.-. ");
+    morse = morse.replace(/D/gi, "-.. ");
+    morse = morse.replace(/E/gi, ". ");
+    morse = morse.replace(/F/gi, "..-. ");
+    morse = morse.replace(/G/gi, "--. ");
+    morse = morse.replace(/H/gi, ".... ");
+    morse = morse.replace(/I/gi, ".. ");
+    morse = morse.replace(/J/gi, ".--- ");
+    morse = morse.replace(/K/gi, "-.- ");
+    morse = morse.replace(/L/gi, ".-.. ");
+    morse = morse.replace(/M/gi, "-- ");
+    morse = morse.replace(/N/gi, "-. ");
+    morse = morse.replace(/O/gi, "--- ");
+    morse = morse.replace(/P/gi, ".--. ");
+    morse = morse.replace(/Q/gi, "--.- ");
+    morse = morse.replace(/R/gi, ".-. ");
+    morse = morse.replace(/S/gi, "... ");
+    morse = morse.replace(/T/gi, "- ");
+    morse = morse.replace(/U/gi, "..- ");
+    morse = morse.replace(/V/gi, "...- ");
+    morse = morse.replace(/W/gi, ".-- ");
+    morse = morse.replace(/X/gi, "-..- ");
+    morse = morse.replace(/Y/gi, "-.-- ");
+    morse = morse.replace(/Z/gi, "--.. ");
+    morse = morse.replace(/  /gi, "/");
 
     //Número
-    morse = morse.replace(/1/gi, ".----");
-    morse = morse.replace(/2/gi, "..---");
-    morse = morse.replace(/3/gi, "...--");
-    morse = morse.replace(/4/gi, "....-");
-    morse = morse.replace(/5/gi, ".....");
-    morse = morse.replace(/6/gi, "-....");
-    morse = morse.replace(/7/gi, "--...");
-    morse = morse.replace(/8/gi, "---..");
-    morse = morse.replace(/9/gi, "----.");
-    morse = morse.replace(/0/gi, "-----");
+    morse = morse.replace(/1/gi, ".---- ");
+    morse = morse.replace(/2/gi, "..--- ");
+    morse = morse.replace(/3/gi, "...-- ");
+    morse = morse.replace(/4/gi, "....- ");
+    morse = morse.replace(/5/gi, "..... ");
+    morse = morse.replace(/6/gi, "-.... ");
+    morse = morse.replace(/7/gi, "--... ");
+    morse = morse.replace(/8/gi, "---.. ");
+    morse = morse.replace(/9/gi, "----. ");
+    morse = morse.replace(/0/gi, "----- ");
     
     resultText.innerText = morse;
 
